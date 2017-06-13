bff7f2ad4f7d2ac188b6cc79f6a4b0e2ebaa5e9e
pospispa <ppospisi@redhat.com>
2017-06-13 19:00:42 +0200

    Shared File Systems: Share Export Locations
    In order to mount a share one needs to know the share export location.
    
    That's why the Get Share Export Locations request is added.

--- openstack/sharedfilesystems/v2/shares/urls.go
+++ openstack/sharedfilesystems/v2/shares/urls.go
@@ -27,3 +27,7 @@ func getMicroversionsURL(c *gophercloud.ServiceClient) string {
 func grantAccessURL(c *gophercloud.ServiceClient, id string) string {
 	return c.ServiceURL("shares", id, "action")
 }
+
+func getExportLocationsURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL("shares", id, "export_locations")
+}
